4c9d778f5278eb45005d3fdcd87c363328c2d513
yiranwangg <wang.yiran@northeastern.edu>
2022-03-01 22:39:02 -0800

    add lab5

---
+++ lab5/src/App.jsx
@@ -0,0 +1,25 @@
+function App(){
+    return(
+      <div class="container">
+  
+          <div class="box header">
+              <h1>Yiran Wang</h1>
+          </div>
+  
+          <div class="box left-side-bar">
+          </div>
+          
+          <div class="box main-area">
+              <div class="hover-box"></div>
+          </div>
+  
+          <div class="box right-side-bar">
+          </div>
+          
+          <div class="box footer">
+          </div>
+  
+      </div>
+    )
+  }
+  export default App;
